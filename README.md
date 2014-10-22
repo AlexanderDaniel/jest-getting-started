d408c8b8fba4b5e736bbf834dcede93aa11c40c6
Alexander Daniel <alexander.daniel@gmx.at>
2014-10-22 18:40:33 +0200

    Working

---
+++ README.md
@@ -0,0 +1,1 @@
+Based on https://facebook.github.io/jest/
---
+++ sum.js
@@ -0,0 +1,6 @@
+// sum.js
+// https://facebook.github.io/jest/
+function sum(value1, value2) {
+    return value1 + value2;
+}
+module.exports = sum;
